14f14273f43691fd9d00536f96877c599fa6950f
chjjh0 <chjjh0@gmail.com>
2018-09-02 23:29:19 +0900

    0902

---
+++ src/main/java/com/gms/web/controller/MemberController.java
@@ -0,0 +1,78 @@
+package com.gms.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.gms.web.domain.MemberDTO;
+import com.gms.web.service.MemberService;
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	@Autowired MemberDTO member;
+	@Autowired MemberService memberService;
+	@RequestMapping(value="/add", method={RequestMethod.GET, RequestMethod.POST})
+	public String add(@ModelAttribute("member") MemberDTO member) {
+		System.out.println("MemberController_add");
+		System.out.println("name is "+member.getName());
+		memberService.add(member);
+		System.out.println("MemberController_add_complete");
+		return "redirect:/move/login/member/login";
+	}
+	@RequestMapping("/list")
+	public void list() {}
+	@RequestMapping("/search")
+	public void search() {}
+	@RequestMapping("/retrieve")
+	public void retrieve() {}
+	@RequestMapping("/count")
+	public void count() {}
+	@RequestMapping(value="/modify/{id}", method={RequestMethod.GET, RequestMethod.POST})
+	public String modify(@PathVariable String id) {
+		System.out.println("MemberController_modify");
+		System.out.println("Id is "+id);
+		return "modify:member/modify.tiles";
+	}
+	@RequestMapping(value="/remove/{id}", method={RequestMethod.GET, RequestMethod.POST})
+	public String remove(@PathVariable String id, @ModelAttribute("member") MemberDTO member, Model model) {
+		System.out.println("MemberController_remove");
+		System.out.println("Pass is "+member.getPass());
+		System.out.println("Id is "+id);
+		member.setMemberId(id);
+		memberService.remove(member);
+		return "redirect:/";
+	}
+	@RequestMapping("/login/{id}/{pass}")
+	public String login(Model model, @PathVariable String id, @PathVariable String pass) {
+		System.out.println("MemberControll_login");
+		System.out.println("id : " +id);
+		System.out.println("pass : " +pass);
+		logger.info("MembeController login");
+		Map<String, String> p = new HashMap<>();
+		p.put("memberId", id);
+		p.put("pass", pass);
+		MemberDTO m = memberService.retrieve(p);
+		model.addAttribute("user", m);
+		System.out.println("getName(): "+m.getName());
+		return "retrieve:member/retrieve.tiles";
+	}
+	@RequestMapping("/logout")
+	public String logout() {
+		logger.info("MembeController logout");
+		return "redirect:/";
+	}
+	@RequestMapping("/fileupload")
+	public void fileupload() {}
+}
---
+++ src/main/webapp/WEB-INF/views/member/retrieve.jsp
@@ -0,0 +1,49 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<script> 
+String s = "${user.memberId}";
+session.setAttribute("usert", s);
+</script>
+<div id="content-box">
+<html>
+<head>
+<script src="${context}/resources/js/app.js"></script>
+</head>
+<body>
+	<table>
+		<tr>
+			<td rowspan="3">
+				<img id="profile" src="${img}${profile}" />
+			</td>
+			<td>아이디</td>
+			<td id="retrieveMemberId" colspan="2">${user.memberId}</td>
+		</tr>
+		<tr>
+			<td>이름</td>
+			<td colspan="2">${user.name}</td>
+		</tr>
+		<tr>
+			<td><a>비밀번호</a></td>
+			<td colspan="2">**********</td>
+		</tr>
+		<tr>
+			<td>나이</td>
+			<td>${user.age}</td>
+			<td><a>팀명</a></td>
+			<td>${user.teamId}</td>
+		</tr>
+		<tr>
+		<td>성별</td>
+		<td>${user.gender}</td>
+		<td><a>역활</a></td>
+		<td>${user.roll}</td>
+		</tr>
+	</table>
+		<a id="moveUpdateForm">정보수정</a> &nbsp; &nbsp; &nbsp;
+		<a id="moveRemoveForm">회원탈퇴</a> &nbsp; &nbsp; &nbsp;
+		<a id="logout">로그아웃</a>
+</div>
+<script>
+app.user.set('${user.memberId}');
+</script>
+</body>
+</html>
